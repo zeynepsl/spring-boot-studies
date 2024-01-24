34534d285e2c4f98066cb75090453dcfe252f308
zsalman <zsalman@protel.com.tr>
2024-01-24 21:08:21 +0300

    initial commit

---
+++ src/main/java/com/zeynep/casestudy/CaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.zeynep.casestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseStudyApplication.class, args);
+	}
+
+}
