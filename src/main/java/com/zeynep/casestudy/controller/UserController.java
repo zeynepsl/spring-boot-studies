8345d43c6942742cc9c1155e898e7a2a94ab9032
zeynepsl <zeyneprsalman@gmail.com>
2024-01-27 21:45:37 +0300

    added notes

--- src/main/java/com/zeynep/casestudy/controller/UserController.java
+++ src/main/java/com/zeynep/casestudy/controller/UserController.java
@@ -7,11 +7,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class UserController {
 
-    @GetMapping("/user")
-    public String getUser(Authentication authentication) {
-        return "Hello Spring Security: " + authentication.getName();
+    /*
+    we create a simple REST API and protect it using Spring Security.
+    Well, if we add Spring security dependency to the Spring boot project then by default Spring Security secures all the application URLs. */
+
+    @GetMapping
+    public String  greetings() {
+        return "Hello world!";
     }
 
-    //source: https://springjavatutorial.medium.com/spring-security-in-memory-authentication-in-spring-boot-a1870db57059
+    @GetMapping("/user")
+    public String getUser(Authentication authentication) {
+        return "Hello Spring Security User: " + authentication.getName();
+    }
+    @GetMapping("/admin")
+    public String getAdmin(Authentication authentication) {
+        return "Hello Spring Security admin: " + authentication.getName();
+    }
+
+    @GetMapping("/hello")
+    public String getAll() {
+        return "Hello Spring Security other users";
+    }
+
+    //sources:
+    // https://springjavatutorial.medium.com/spring-security-in-memory-authentication-in-spring-boot-a1870db57059
+    // https://www.javaguides.net/2023/04/spring-security-tutorial-in-memory-authentication.html
+    // https://medium.com/@ritesh.panigrahi/spring-security-in-memory-authentication-and-authorization-dcb9cc8baf19
 
 }
