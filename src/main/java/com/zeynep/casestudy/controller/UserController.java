96ebbaac7cf7730f6df9ad49a1516d4925fcda47
zsalman <zsalman@protel.com.tr>
2024-01-27 21:01:31 +0300

    added ref

--- src/main/java/com/zeynep/casestudy/controller/UserController.java
+++ src/main/java/com/zeynep/casestudy/controller/UserController.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class UserController {
+
     @GetMapping("/user")
     public String getUser(Authentication authentication) {
         return "Hello Spring Security: " + authentication.getName();
     }
+
+    //source: https://springjavatutorial.medium.com/spring-security-in-memory-authentication-in-spring-boot-a1870db57059
+
 }
