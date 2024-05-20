90fcb1d1656b6fbd6b011c957096c5f8879d5298
zeynepsl <zeyneprsalman@gmail.com>
2024-05-20 14:34:42 +0300

    upd

--- src/main/java/com/zeynep/casestudy/CaseStudyApplication.java
+++ src/main/java/com/zeynep/casestudy/CaseStudyApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy(proxyTargetClass = true)
-//aop kullandıgımız soyluyoruz springe xml confige de ekleniyor
+@EnableAspectJAutoProxy(proxyTargetClass = true)//aop kullandıgımız soyluyoruz springe xml confige de ekleniyor
 public class CaseStudyApplication {
 
     public static void main(String[] args) {
